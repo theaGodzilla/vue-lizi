3217fa1363d270affe2bfbe692bd0fb30ba8c07a
theaGodzilla <464843022@qq.com>
2019-01-19 12:02:16 +0800

    详情更新

--- lizi/src/components/detail.vue
+++ lizi/src/components/detail.vue
@@ -10,33 +10,37 @@
             <main>
                 <div class="con-img">
                     <div class="main-img">
+                        <!-- lists.image_url_set.single_many[0][0] -->
                         <img :src="lists.img" alt="">
                     </div>
                     <div class="slide-left-icon">
-                        <img :src="thelist.area_icon_v2" alt="">
+                        <img :src="thelist.area_icon_v2?thelist.area_icon_v2:thelist.tag" alt="">
                     </div>
                 </div>
                 <div class="price">
-                    <span>￥{{lists.jumei_price}}</span>
-                    <span>价格详情</span>
-                    <span>{{lists.buyer_number_text}}</span>
+                    <span class="cur_price">￥{{lists.jumei_price}}</span>
+                    <del class="cost_price">￥{{lists.market_price}}</del>
+                    <span class="detail_text">价格详情</span>
+                    <span class="cur_num">{{lists.buyer_number_text}}</span>
                 </div>
-                <div class="list-name">
-                    <span>{{thelist.special_tags.jm_owner?thelist.special_tags.jm_owner:''}}</span>
+                <div class="normal_desc">
+                    <span class="desc_iconv2">{{thelist.special_tags?thelist.special_tags.jm_owner:''}}</span>
                     {{thelist.qrshare_product_name}}
                 </div>
+                <!-- 说明和运费 -->
                 <div class="additional-info">
-                    <div class="">
+                    <div class="postage-wrap">
                         <label>运费</label>
-                        <span>{{lists.freight.text}}</span>
+                        <span class="">{{lists.freight?lists.freight.text:''}}</span>
                     </div>
-                    <div>
+                    <div class="introduction-wrap">
                         <label>说明</label>
-                        <span>{{lists.icon_tag[0].name}}</span>
-                        <span>{{lists.icon_tag[1].name}}</span>
-                        <span>{{lists.icon_tag[2].name}}</span>
-                        <span>{{lists.icon_tag[3].name}}</span>
+                        <span v-for="item in lists.icon_tag" :key="item.label" :style="{display:item.name?'inline-block':'none'}"><i class="iconfont icon-pingjia"></i>{{item.name}}</span>
                     </div>
+                </div>
+                <!-- 型号分类 -->
+                <div class="page_sku_wrapper">
+
                 </div>
             </main>
             <footer>
@@ -120,17 +124,89 @@ export default {
             }
         }
         .price{
-            padding:0 rem(10px);
+            padding:rem(15px) rem(14px) 0 rem(14px); 
+            height: rem(33px);
+            .cur_price{
+                float: left;
+                color: #f33873;
+                font-size: rem(28px);
+                line-height: rem(33px);
+            }
+            .cost_price{
+                float: left;
+                color: #666;
+                font-size: rem(13px);
+                margin-left: rem(8px);
+                margin-top: rem(8px);
+            }
+            .detail_text{
+                float: left;
+                color: #666;
+                font-size: rem(15px);
+                margin-left: rem(8px);
+                line-height: rem(33px);
+            }
+            .cur_num{
+                float: right;
+                line-height: rem(33px);
+            }
         }
-        .list-name{
+        .normal_desc{
+            line-height: rem(26px);
+            padding: rem(15px) rem(14px) rem(10px) rem(14px);
+            // line-height: rem(12px);
+            font-size: rem(15px);
+            // width: 20rem;
+            // -webkit-box-sizing: border-box;
+            box-sizing: border-box;
+            .desc_iconv2{
+                margin-right: rem(4px);
+                color: white;
+                font-size: rem(10px);
+                padding: 1px rem(4px);
+                background: #fe4070;
+                border-radius: rem(4px);
+                -webkit-box-sizing: border-box;
+                box-sizing: border-box;
+            }
 
         }
         .additional-info{
+            padding: 0 rem(14px) rem(10px) rem(14px);
+            label{
+                line-height: rem(13px);
+                width: 22%;
+                color: #999;
+                display: inline-block;
+                font-size: rem(15px);
+                // vertical-align: top;
+                text-align: center;
+                float: left;
+                padding: rem(12px) 0;
+            }
+            .postage-wrap{
+                border-bottom: 1px solid #eee;
+                padding: rem(8px) 0;
+            }
+            .introduction-wrap{
+                padding: rem(8px) 0;
+                span{
+                    padding-right: rem(8px);
+                    .icon-pingjia{
+                        color: #f33873;
+                    }
+                }
+            }
             
+        }
+        .page_sku_wrapper{
+            padding: rem(15px) 0;
+            background: #eee;
         }
     }
     footer{
-
+        height: rem(44px);
+        width: 100%;
     }
 }
 </style>
