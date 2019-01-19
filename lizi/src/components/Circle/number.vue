cc95bb82f8c96e77944f286753ae24f26ea52992
aaa <1232@qq.com>
2019-01-19 14:27:52 +0800

    list列表无限加载

---
+++ lizi/src/components/Circle/number.vue
@@ -0,0 +1,12 @@
+<template>
+<div class="Number">
+    <!-- <h1>number</h1> -->
+  </div>
+</template>
+
+<script>
+export default {
+    name:'Number',
+    
+}
+</script>
---
+++ lizi/src/components/Circle/price.vue
@@ -0,0 +1,12 @@
+<template>
+<div class="Price">
+    <h1>price</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'Price',
+   
+}
+</script>
